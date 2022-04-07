b0548c66023b8f8a7a632e41dfd9bcec77879ff5
v.ropokis <v.ropokis@jayex.com>
2022-04-07 05:30:14 +0100

    initial comit

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const mysql = require("mysql");
+const session = require("express-session");
+const mySQL_store = require("express-mysql-session")(session);
+const Router = require("./router");
+
+app.use(express.static(path.join(__dirname, "build")));
+app.use(express.json());
+
+// Database
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "loginapp",
+});
+
+db.connect(function (err) {
+  if (err) {
+    console.log("Databse had an issue");
+    throw err;
+    return false;
+  }
+});
+
+// Sessions
+const session_store = new mySQL_store(
+  {
+    expiration: 300000000,
+    endConnectionOnClose: false,
+  },
+  db
+);
+
+app.use(
+  session({
+    key: "32332sdadsfa232asd",
+    secret: "dfas232324fcdsfa",
+    store: session_store,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 300000000,
+      httpOnly: false,
+    },
+  })
+);
+
+new Router(app, db);
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(3000);
---
+++ router.js
@@ -0,0 +1,258 @@
+const bcrypt = require("bcrypt");
+
+class Router {
+  constructor(app, db) {
+    this.login(app, db);
+    this.logout(app, db);
+    this.isLoggedIn(app, db);
+    this.findUsername(app, db);
+    this.register(app, db);
+    this.passwordReset(app, db);
+  }
+
+  login(app, db) {
+    app.post("/login", (req, res) => {
+      let username = req.body.username;
+      let password = req.body.password;
+
+      username = username.toLowerCase();
+
+      if (username.length > 12 || password.length > 12) {
+        res.json({
+          success: false,
+          message: "Username or password is incorrect, please try again.",
+        });
+        return;
+      }
+
+      let cols = [username];
+      db.query(
+        "SELECT * FROM user WHERE username = ? LIMIT 1",
+        cols,
+        (err, data, fields) => {
+          if (err) {
+            res.json({
+              success: false,
+              message: "An error has occured, please try again.",
+            });
+            return;
+          }
+
+          // User found
+          if (data && data.length === 1) {
+            bcrypt.compare(
+              password,
+              data[0].password,
+              (bcryptErr, verified) => {
+                if (verified) {
+                  req.session.userID = data[0].id;
+                  res.json({
+                    success: true,
+                    username: data[0].username,
+                  });
+                  return;
+                } else {
+                  res.json({
+                    success: false,
+                    message:
+                      "Username or password is incorrect, please try again.",
+                  });
+                }
+              }
+            );
+          } else {
+            res.json({
+              success: false,
+              message: "Username or password is incorrect, please try again.",
+            });
+          }
+        }
+      );
+    });
+  }
+
+  logout(app, db) {
+    app.post("/logout", (req, res) => {
+      if (req.session.userID) {
+        req.session.destroy();
+        res.json({
+          success: true,
+        });
+        return true;
+      } else {
+        res.json({
+          sucess: false,
+        });
+        return false;
+      }
+    });
+  }
+
+  isLoggedIn(app, db) {
+    app.post("/isLoggedIn", (req, res) => {
+      if (req.session.userID) {
+        let cols = [req.session.userID];
+        db.query(
+          "SELECT * FROM user WHERE id = ? LIMIT 1",
+          cols,
+          (err, data, fields) => {
+            if (data && data.length === 1) {
+              res.json({
+                success: true,
+                username: data[0].username,
+              });
+              return true;
+            } else {
+              res.json({
+                sucesss: false,
+              });
+            }
+          }
+        );
+      } else {
+        res.json({
+          sucesss: false,
+        });
+      }
+    });
+  }
+
+  findUsername(app, db) {
+    app.post("/findUsername", (req, res) => {
+      let username = req.body.username;
+
+      username = username.toLowerCase();
+
+      if (username.length > 12) {
+        res.json({
+          success: false,
+          message: "Username or password is incorrect, please try again.",
+        });
+        return;
+      }
+
+      let cols = [username];
+      db.query(
+        "SELECT * FROM user WHERE username = ? LIMIT 1",
+        cols,
+        (err, data, fields) => {
+          if (err) {
+            res.json({
+              success: false,
+              message: "An error has occured, please try again.",
+            });
+            return;
+          }
+
+          // User found
+          if (data && data.length === 1) {
+            res.json({
+              success: true,
+              username: data[0].username,
+            });
+            return;
+          } else {
+            res.json({
+              success: false,
+              message: "",
+            });
+          }
+        }
+      );
+    });
+  }
+
+  register(app, db) {
+    app.post("/register", (req, res) => {
+      let username = req.body.username;
+      let password = req.body.password;
+
+      username = username.toLowerCase();
+
+      if (username.length > 12 || password.length > 12) {
+        res.json({
+          success: false,
+          message: "Username or password is incorrect, please try again.",
+        });
+        return;
+      }
+      password = bcrypt.hashSync(password, 9);
+      let cols = [[username, password]];
+      db.query(
+        "INSERT INTO user (username, password) VALUES ?",
+        [cols],
+        (err, data, fields) => {
+          if (err) {
+            console.log(err);
+            res.json({
+              success: false,
+              message: "An error has occured, please try again.",
+            });
+            return;
+          }
+          // User found
+          if (data && data.length === 1) {
+            req.session.userID = data[0].id;
+            res.json({
+              success: true,
+              username: data[0].username,
+            });
+          } else {
+            res.json({
+              success: false,
+              message: "Username or password is incorrect, please try again.",
+            });
+          }
+        }
+      );
+    });
+  }
+
+  passwordReset(app, db) {
+    app.post("/passwordReset", (req, res) => {
+      let username = req.body.username;
+      let password = req.body.password;
+
+      username = username.toLowerCase();
+
+      if (username.length > 12 || password.length > 12) {
+        res.json({
+          success: false,
+          message: "Username or password is incorrect, please try again.",
+        });
+        return;
+      }
+      password = bcrypt.hashSync(password, 9);
+      let cols = [password, username];
+      db.query(
+        `UPDATE user SET password = ? WHERE username = ?`,
+        cols,
+        (err, data, fields) => {
+          if (err) {
+            console.log(err);
+            res.json({
+              success: false,
+              message: "An error has occured, please try again.",
+            });
+            return;
+          }
+          // User found
+          if (data && data.length === 1) {
+            req.session.userID = data[0].id;
+            res.json({
+              success: true,
+              username: data[0].username,
+            });
+          } else {
+            res.json({
+              success: false,
+              message: "Username or password is incorrect, please try again.",
+            });
+          }
+        }
+      );
+    });
+  }
+}
+
+module.exports = Router;
